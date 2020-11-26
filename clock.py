d250fec2844c0243db6df30d8af4721a6608924b
Claudia <clau@Claudias-MacBook-Pro.local>
2020-11-25 22:47:56 -0800

    Add tweet time

--- clock.py
+++ clock.py
@@ -1,11 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from main import tweet_headlines
+import datetime as dt
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=12)
 def scheduled_job():
 	tweet_headlines()
-	print('Tweeted')
+	print('Tweeted at ', dt.datetime.today())
 
 sched.start()
